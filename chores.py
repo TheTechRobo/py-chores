5020552ed87b69086e8e6512f64bacec75113814
Ittussarom Retals Mail Ynohtna <52163910+TheTechRobo@users.noreply.github.com>
2020-01-27 12:43:29 -0500

    Create chores.py
    not done yet (duh)

---
+++ chores.py
@@ -0,0 +1,11 @@
+"""
+choreSnake
+version N/A
+"""
+print("Welcome to choreSnake!")
+print("Loading...")
+def allowance(person, chore):
+    print("Loading...")
+    if person == person01:
+        #TO DO: get data from file allowance01, else create new file allowance01 with balance of 0
+        print("Current allowance for Person 01" is ", allowance01)
